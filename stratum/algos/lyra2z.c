920ed9e649b193326eadbe67b5f1777a2596a35b
lanciaominer <43009244+lanciaominer@users.noreply.github.com>
2018-09-06 00:38:39 +0800

    Update lyra2z.c

--- stratum/algos/lyra2z.c
+++ stratum/algos/lyra2z.c
@@ -11,7 +11,7 @@
 
 extern uint64_t lyra2z_height;
 
-void lyra2z_hash(const char* input, char* output, uint32_t len)
+void lyra2z_hash(const char* input, char* output, int size)
 {
 	uint32_t _ALIGN(64) hashB[8], hash[8];
 	sph_blake256_context ctx_blake;
@@ -29,7 +29,7 @@ void lyra2z_hash(const char* input, char* output, uint32_t len)
 	sph_blake256_set_rounds(14);
 
 	sph_blake256_init(&ctx_blake);
-	sph_blake256(&ctx_blake, input, len);
+	sph_blake256(&ctx_blake, input, size);
 	sph_blake256_close(&ctx_blake, hashB);
 
 	LYRA2z(hash, 32, hashB, 32, hashB, 32, 8, 8, 8);
