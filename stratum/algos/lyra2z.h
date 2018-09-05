ffc23e832c0a46e9903b749e406d5442e6df972c
lanciaominer <43009244+lanciaominer@users.noreply.github.com>
2018-09-06 00:35:04 +0800

    Update lyra2z.h

--- stratum/algos/lyra2z.h
+++ stratum/algos/lyra2z.h
@@ -7,7 +7,7 @@ extern "C" {
 
 #include <stdint.h>
 
-void lyra2z_hash(const char* input, char* output, uint32_t len);
+void lyra2z_hash(const char* input, char* output, int size);
 
 #ifdef __cplusplus
 }
